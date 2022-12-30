9123330353fccbc6b6e5e8e1c0835721ea33634a
Sumit <sumit@kodnest.com>
2022-12-30 10:13:14 +0530

    footer fixed

--- components/Collection/index.jsx
+++ components/Collection/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function index({ children }) {
   return (
-    <div className="cursor-pointer relative group overflow-hidden w-full h-[868px]">
+    <div className="cursor-pointer relative group overflow-hidden w-full h-[465px] sm:h-[868px]">
       {children}
     </div>
   );
